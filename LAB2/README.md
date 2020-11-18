d612c0f49a9ccbe23fcd154fa37f88c64b41e287
MetGang <patrick_stritch@wp.pl>
2020-11-19 00:59:55 +0100

    Add basic second Selenium test

--- LAB2/README.md
+++ LAB2/README.md
@@ -4,16 +4,18 @@
 
 **Wyszukanie i odtworzenie losowego filmu w 4K**
 
-Użytkownik otwiera dowolną, zmaksymalizowaną przeglądarkę, wpisuje adres `vimeo.com` i klika enter. Po wejściu na stronę klika przycisk lupy i wpisuje frazę `4K` w pole wyszukiwania po czym klika strzałkę, aby rozpocząć wyszukiwanie. Po przejściu na kolejną stronę, użytkownik klika pierwszą miniaturkę filmu i przechodzi do strony filmu. Gdy się już tam znajdzie, klika zębatkę w pasku odtwarzacza, zaznacza jakość `2160p` po czym przechodzi w tryb pełnoekranowy.
+Użytkownik otwiera dowolną, zmaksymalizowaną przeglądarkę, wpisuje adres `vimeo.com` i klika enter. Po wejściu na stronę klika przycisk lupy i wpisuje frazę `4K` w pole wyszukiwania po czym klika przycisk strzałki, aby rozpocząć wyszukiwanie. Po przejściu na kolejną stronę, użytkownik klika pierwszą miniaturkę filmu i przechodzi do strony filmu. Gdy się już tam znajdzie, klika zębatkę w pasku odtwarzacza, zaznacza jakość `2160p` po czym przechodzi w tryb pełnoekranowy.
 
 ### Scenariusz 2
 
 **Wyszukanie i odtworzenie losowej animacji o tematyce Vimeo**
 
-Użytkownik otwiera dowolną, zmaksymalizowaną przeglądarkę, wpisuje adres `vimeo.com` i klika enter. Po wejściu na stronę klika przycisk lupy i wpisuje frazę `vimeo` w pole wyszukiwania po czym klika strzałkę, aby rozpocząć wyszukiwanie. Po przejściu na kolejną stronę, użytkownik wybiera kategorię animacji po czym klika trzecią miniaturkę filmu.
+Użytkownik otwiera dowolną, zmaksymalizowaną przeglądarkę, wpisuje adres `vimeo.com` i klika enter. Po wejściu na stronę klika przycisk lupy i wpisuje frazę `vimeo` w pole wyszukiwania po czym klika przycisk strzałki, aby rozpocząć wyszukiwanie. Po przejściu na kolejną stronę, użytkownik wybiera kategorię animacji po czym klika trzecią miniaturkę filmu.
 
 # Test 2
 
 ### Scenariusz 1
+
+Użytkownik otwiera dowolną, zmaksymalizowaną przeglądarkę, wpisuje adres `allegro.pl` i klika enter. Po wejściu na stronę wpisuje frazę `kostka rubika` w pole wyszukiwania po czym klika przycisk `Szukaj`. Po przejściu na kolejną stronę, użytkownik wybiera pierwszą promowaną ofertę, dodaje ją do koszyka, do którego potem przechodzi i klika przycisk dotyczący metody płatności. Jeśli użytkownik nie był wcześniej zalogowany - proszony jest o zalogowanie.
 
 ### Scenariusz 2
--- LAB2/test.py
+++ LAB2/test.py
@@ -30,15 +30,17 @@ logger.addHandler(stream)
 # ----------------------------------------------------------------
 
 def get_element(driver, by, data):
+    time.sleep(2)
     return WebDriverWait(driver, 16).until(expected_conditions.presence_of_element_located((by, data)))
 
 def get_elements(driver, by, data):
+    time.sleep(2)
     return WebDriverWait(driver, 16).until(expected_conditions.presence_of_all_elements_located((by, data)))
 
 # ----------------------------------------------------------------
 
 def test_1a(driver):
-    logger.info('Test pierwszy, scenariusz pierwszy')
+    logger.info('Test 1. scenariusz 1.')
 
     logger.info('Przechodzę na stronę Vimeo')
     driver.get('https://vimeo.com/')
@@ -100,7 +102,7 @@ def test_1a(driver):
 # ---------------------------------------------------------------
 
 def test_1b(driver):
-    logger.info('Test pierwszy, scenariusz drugi')
+    logger.info('Test 1. scenariusz 2.')
 
     logger.info('Przechodzę na stronę Vimeo')
     driver.get('https://vimeo.com/')
@@ -131,7 +133,7 @@ def test_1b(driver):
     try:
         get_element(driver, By.CSS_SELECTOR, '[data-value="animation"').click()
     except:
-        logger.error('A')
+        logger.error('Brak kategorii filmów animowanych')
         return False
 
     logger.info('Klikam miniaturkę filmu')
@@ -147,8 +149,62 @@ def test_1b(driver):
 
 # ---------------------------------------------------------------
 
-def test_2(driver):
-    logger.info('Test drugi')
+def test_2a(driver):
+    logger.info('Test 2. scenariusz 1.')
+
+    logger.info('Przechodzę na stronę Allegro')
+    driver.get('https://allegro.pl/')
+    logger.info('Jestem na stronie "' + driver.title + '"')
+
+    logger.info('Zamykam powiedomienie o ciasteczkach')
+    try:
+        get_element(driver, By.CSS_SELECTOR, 'button[data-role="close-and-accept-consent"]').click()
+    except:
+        logger.warn('Brak powiadomienia o ciasteczkach')
+
+    logger.info('Wprowadzam frazę do szukania')
+    try:
+        get_element(driver, By.CSS_SELECTOR, 'input[data-role="search-input"]').send_keys('kostka rubika')
+    except:
+        logger.error('Brak pola wprowadzania')
+        return False
+
+    logger.info('Klikam przycisk wyszukiwania')
+    try:
+        get_element(driver, By.CSS_SELECTOR, 'button[data-role="search-button"]').click()
+    except:
+        logger.error('Brak przycisku wyszukiwania')
+        return False
+
+    logger.info('Wybieram pierwszą promowaną ofertę')
+    try:
+        get_elements(driver, By.CSS_SELECTOR, 'a[href*="https://allegro.pl/oferta"][class]')[0].click()
+    except:
+        logger.error('Brak oferty do wyboru')
+        return False
+
+    logger.info('Dodaję wybrany produkt do koszyka')
+    try:
+        get_element(driver, By.ID, 'add-to-cart-button').click()
+    except:
+        logger.error('Brak przycisku dodania do koszyka')
+        return False
+
+    logger.info('Przechodzę do koszyka')
+    try:
+        get_element(driver, By.CSS_SELECTOR, 'a[data-analytics-click-label="goToCart"]').click()
+    except:
+        logger.error('Brak przycisku przejścia do koszyka')
+        return False
+
+    logger.info('Klikam przycisk wyboru płatności')
+    try:
+        get_elements(driver, By.TAG_NAME, 'submit-button')[0].click()
+    except:
+        logger.error('Brak przycisku wyboru płatności')
+        return False
+
+    time.sleep(5)
 
     return True
 
@@ -168,7 +224,7 @@ with Edge(options = edgeOptions) as driver:
     logger.info('Testowanie w Microsoft Edge')
     logger.info('Test 1. scenariusz 1. zakończony ' + ('sukcesem' if test_1a(driver) else 'porażką'))
     logger.info('Test 1. scenariusz 2. zakończony ' + ('sukcesem' if test_1b(driver) else 'porażką'))
-    logger.info('Test 2. zakończony ' + ('sukcesem' if test_2(driver) else 'porażką'))
+    logger.info('Test 2. scenariusz 1. zakończony ' + ('sukcesem' if test_2a(driver) else 'porażką'))
 
 # ----------------------------------------------------------------
 
@@ -181,7 +237,7 @@ with webdriver.Chrome(executable_path = 'chromedriver.exe') as driver:
     logger.info('Testowanie w Google Chrome')
     logger.info('Test 1. scenariusz 1. zakończony ' + ('sukcesem' if test_1a(driver) else 'porażką'))
     logger.info('Test 1. scenariusz 2. zakończony ' + ('sukcesem' if test_1b(driver) else 'porażką'))
-    logger.info('Test 2. zakończony ' + ('sukcesem' if test_2(driver) else 'porażką'))
+    logger.info('Test 2. scenariusz 1. zakończony ' + ('sukcesem' if test_2a(driver) else 'porażką'))
 
 # ----------------------------------------------------------------
 
@@ -194,4 +250,4 @@ with webdriver.Firefox(executable_path = 'geckodriver.exe') as driver:
     logger.info('Testowanie w Mozilla Firefox')
     logger.info('Test 1. scenariusz 1. zakończony ' + ('sukcesem' if test_1a(driver) else 'porażką'))
     logger.info('Test 1. scenariusz 2. zakończony ' + ('sukcesem' if test_1b(driver) else 'porażką'))
-    logger.info('Test 2. zakończony ' + ('sukcesem' if test_2(driver) else 'porażką'))
+    logger.info('Test 2. scenariusz 1. zakończony ' + ('sukcesem' if test_2a(driver) else 'porażką'))
